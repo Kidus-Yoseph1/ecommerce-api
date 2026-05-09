c76701bd6dcac2f0b1f20cc1a9c32811202c9546
Kidus-Yoseph1 <kidusy317@gmail.com>
2026-05-09 10:49:56 +0300

    fix: fix a typo

--- internal/domain/payment.go
+++ internal/domain/payment.go
@@ -3,13 +3,13 @@ package domain
 import "time"
 
 type Payment struct {
-	Id                       string    `json:"id"`
-	OrderId                  string    `json:"user_id"`
-	Stripe_payment_intent_id string    `json:"stripe_payment_intent_id"`
-	Amount                   float64   `json:"amount"`
-	Status                   string    `json:"status"`
-	CreatedAt                time.Time `json:"created_at"`
-	UpdatedAt                time.Time `json:"updated_at"`
+	Id                    string    `json:"id"`
+	OrderId               string    `json:"user_id"`
+	StripePaymentIntentID string    `json:"stripe_payment_intent_id"`
+	Amount                float64   `json:"amount"`
+	Status                string    `json:"status"`
+	CreatedAt             time.Time `json:"created_at"`
+	UpdatedAt             time.Time `json:"updated_at"`
 }
 
 type PaymentRepository interface {
