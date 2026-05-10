0d5b4113e5fbb4ee77c5ad5ae08b8604e1c71b28
Kidus-Yoseph1 <kidusy317@gmail.com>
2026-05-10 10:03:45 +0300

    feat: implement register and login services

---
+++ internal/service/auth_service.go
@@ -0,0 +1,76 @@
+package service
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/kidus-yoseph1/ecommerce-api/internal/domain"
+)
+
+type AuthService struct {
+	userRepo  domain.UserRepository
+	jwtSecret string
+}
+
+func NewAuthService(userRepo domain.UserRepository, jwtSecret string) *AuthService {
+	return &AuthService{
+		userRepo:  userRepo,
+		jwtSecret: jwtSecret,
+	}
+}
+
+func (s *AuthService) Register(fullName, email, password string) error {
+	existing, err := s.userRepo.GetUserByEmail(email)
+	if err != nil {
+		return domain.ErrInternal("something went wrong")
+	}
+
+	if existing != nil {
+		return domain.ErrBadRequest("email already in use")
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return domain.ErrInternal("could not hash password")
+	}
+
+	user := domain.User{
+		FullName:     fullName,
+		Email:        email,
+		PasswordHash: string(hash),
+		Role:         "Customer",
+	}
+
+	return s.userRepo.CreateUser(user)
+}
+
+func (s *AuthService) Login(email, password string) (string, error) {
+	user, err := s.userRepo.GetUserByEmail(email)
+	if err != nil {
+		return "", domain.ErrInternal("something went wrong")
+	}
+	if user == nil {
+		return "", domain.ErrBadRequest("invalid email or password")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if err != nil {
+		return "", domain.ErrBadRequest("invalid email or password")
+	}
+
+	claims := jwt.MapClaims{
+		"user_id": user.Id,
+		"role":    user.Role,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	signed, err := token.SignedString([]byte(s.jwtSecret))
+	if err != nil {
+		return "", domain.ErrInternal("could not create token")
+	}
+
+	return signed, nil
+}
